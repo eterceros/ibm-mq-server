faaea152e1568c55ffa6e76891c55e658488c957
Edson Terceros <edson.terceros@avantica.net>
2019-07-03 09:51:04 -0400

    mock Lpid changes

--- src/main/java/com/labcorp/ibmmq/IbmMqApplication.java
+++ src/main/java/com/labcorp/ibmmq/IbmMqApplication.java
@@ -5,16 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 @EnableJms
 public class IbmMqApplication {
 
+    private static final String DESTINATION_QUEUE = "DEV.QUEUE.1";
+    private static final String MIN_KEY = "min";
+    private static final String MAX_KEY = "max";
     @Autowired
     private JmsTemplate jmsTemplate;
 
@@ -25,29 +30,29 @@ public class IbmMqApplication {
     @GetMapping("send")
     String send() {
         try {
-            jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+            Map<String, Integer> lpidChangeMap = generateMinMax();
+            String message = String.format("oldLPID:%s newLPID:%s", lpidChangeMap.get(MIN_KEY).toString(),
+                    lpidChangeMap.get(MAX_KEY).toString());
+            jmsTemplate.convertAndSend(DESTINATION_QUEUE, message);
             return "OK";
         } catch (JmsException ex) {
             ex.printStackTrace();
             return "FAIL";
         }
     }
-    //call lpid update
 
-    @GetMapping("recv")
-    String recv() {
-        try {
-            return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
-        } catch (JmsException ex) {
-            ex.printStackTrace();
-            return "FAIL";
-        }
+    private Map<String, Integer> generateMinMax() {
+        int random1 = generateRandom();
+        int random2 = generateRandom();
+        Map<String, Integer> lpidMap = new HashMap<>();
+        lpidMap.put(MIN_KEY, Math.min(random1, random2));
+        lpidMap.put(MAX_KEY, Math.max(random1, random2));
+        return lpidMap;
     }
 
-    @JmsListener(destination = "DEV.QUEUE.1")
-    public void handleMessage(String message) {//implicit message type conversion
-        System.out.println("received: " + message);
-        //todo see what happens if in the middle the sender goes down and if it goes alive again check if listener
-        // still works
+    private Integer generateRandom() {
+        int min = 1000;
+        int max = 10000;
+        return min + (int) (Math.random() * ((max - min) + 1));
     }
 }
